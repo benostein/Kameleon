16dcda7a65246c5a009b56648027632bb3cd0bfc
BenjiManCan <ben.stein@hotmail.co.uk>
2018-09-15 23:09:55 +0100

    Added navigation bar - toolbar missing

--- app/src/main/java/com/example/ben/kameleon/MainActivity.java
+++ app/src/main/java/com/example/ben/kameleon/MainActivity.java
@@ -1,20 +1,63 @@
 package com.example.ben.kameleon;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Sets custom toolbar as default toolbar and imports menu icon
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        if (getSupportActionBar() != null) {
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//            getSupportActionBar().setLogo(R.drawable.ic_menu);
+//            getSupportActionBar().setDisplayUseLogoEnabled(true);
+//        }
+
+        // Creates navigation drawer
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
     }
 
-    //Commit
-    //Committed to development
-
-    private void TestCommit() {
-        int i = 1+1;
+    // Allows menu button to open navigation panel when pressed
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
