6becc736a38978a61a87621c8a97ed2c1b7bd969
BenjiManCan <ben.stein@hotmail.co.uk>
2018-09-18 18:41:18 +0100

    Changed colours and added navigation drawer image

--- app/src/main/java/com/example/ben/kameleon/MainActivity.java
+++ app/src/main/java/com/example/ben/kameleon/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.ben.kameleon;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
 
@@ -31,21 +33,25 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        menuItem.setChecked(true);
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
+        navigationView.setNavigationItemSelectedListener(this);
 
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, 't', 't');
+//        mDrawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
 
-                        return true;
-                    }
-                });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()){
+////            case R.id.nav_settings:
+////                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+////                break;
+////        }
+////
+////        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     // Allows menu button to open navigation panel when pressed
@@ -58,4 +64,13 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+//    @Override
+//    public void onBackPressed() {
+//        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+//            mDrawerLayout.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
 }
