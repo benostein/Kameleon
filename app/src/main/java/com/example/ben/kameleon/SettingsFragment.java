997bc3667e29a9eb9abebbba3c70f208d7f06bb5
BenjiManCan <ben.stein@hotmail.co.uk>
2018-10-11 12:19:17 +0100

    Spinner values in settings and widgets import from array.

--- app/src/main/java/com/example/ben/kameleon/SettingsFragment.java
+++ app/src/main/java/com/example/ben/kameleon/SettingsFragment.java
@@ -9,6 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class SettingsFragment extends Fragment {
     @Nullable
@@ -17,6 +21,26 @@ public class SettingsFragment extends Fragment {
 
         // When view is being created, do this:
 
+        // Gets the current view on the device and allows for easier access of resources
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // Finds the settings spinner in the XML and gives it a variable to access
+        Spinner settingsSpinner = v.findViewById(R.id.temp_setting_spinner);
+
+        // Defines new array list
+        ArrayList<String> settingsArray = new ArrayList<>();
+
+        // Adds each item to the settings array
+        settingsArray.add("Celsius");
+        settingsArray.add("Fahrenheit");
+        settingsArray.add("Kelvin");
+
+        // Creates an adapter that allows an array to be used as the contents of the spinner
+        ArrayAdapter<String> settingsAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, settingsArray);
+        // Uses the adapter to set the values of the array to the contents and style of the spinner
+        settingsAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        settingsSpinner.setAdapter(settingsAdapter);
+        
         // Sets the action bar at the top of the app to say the current mode
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Settings");
 
--- app/src/main/java/com/example/ben/kameleon/WidgetsFragment.java
+++ app/src/main/java/com/example/ben/kameleon/WidgetsFragment.java
@@ -7,18 +7,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class WidgetsFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         // When view is being created, do this:
 
+        // Gets the current view on the device and allows for easier access of resources
+        View v = inflater.inflate(R.layout.fragment_widgets, container, false);
+
+        // Finds the widget spinner in the XML and gives it a variable to access
+        Spinner widgetSpinner = v.findViewById(R.id.weather_widget_spinner);
+
+        // Defines new array list
+        ArrayList<String> widgetArray = new ArrayList<>();
+
+        // Adds each item to the widget array
+        widgetArray.add("None");
+        widgetArray.add("Temperature");
+        widgetArray.add("Humidity");
+        widgetArray.add("Wind Speed");
+
+        // Creates an adapter that allows an array to be used as the contents of the spinner
+        ArrayAdapter<String> widgetAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, widgetArray);
+        // Uses the adapter to set the values of the array to the contents and style of the spinner
+        widgetAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        widgetSpinner.setAdapter(widgetAdapter);
+
         // Sets the action bar at the top of the app to say the current mode
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Widgets");
 
-        return inflater.inflate(R.layout.fragment_widgets, container, false);
+        return v;
     }
 
     @Override
