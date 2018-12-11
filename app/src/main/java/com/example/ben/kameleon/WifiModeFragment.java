f2857cebfd803ea4acb6b4a7895e6b655e96bdee
BenjiManCan <ben.stein@hotmail.co.uk>
2018-12-11 21:28:16 +0000

    Started to implement RecyclerView in Wi-Fi mode.

--- app/src/main/java/com/example/ben/kameleon/WifiModeFragment.java
+++ app/src/main/java/com/example/ben/kameleon/WifiModeFragment.java
@@ -1,20 +1,46 @@
 package com.example.ben.kameleon;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class WifiModeFragment extends Fragment implements View.OnClickListener {
+
 
     @Nullable
     @Override
@@ -67,12 +93,16 @@ public class WifiModeFragment extends Fragment implements View.OnClickListener {
             }
         });
 
+
         return v;
     }
 
     public void onClick(View view) {
 
+    }
 
+    public class ItemModel {
+        public String itemLabel;
     }
 
     @Override
@@ -80,6 +110,10 @@ public class WifiModeFragment extends Fragment implements View.OnClickListener {
         super.onViewCreated(view, savedInstanceState);
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
 
     public void replaceFragment(Fragment someFragment) {
